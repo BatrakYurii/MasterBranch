2148d20f1b8988146c5c8142c4378f8f8ac15e15
Batrak Yurii <navid7498@gmail.com>
2021-09-20 20:23:15 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MasterBranch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
